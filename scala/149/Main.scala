c1ed009ad41c12fd6be91bb6579940c6ed151888
Jordy Moos <jordy@pbwebmedia.com>
2016-05-19 14:14:21 +0200

    Challenge 149

--- scala/149/Main.scala
+++ scala/149/Main.scala
@@ -1,14 +1,14 @@
 object Main extends App {
-  def consume(input: Array[String], output: Array[String], number: String): Array[String] = {
-    flag(input.tail, output :+ (number * input.head.length))
+  def consumeNumber(input: Array[String], output: Array[String], number: String): Array[String] = {
+    consumeFlag(input.tail, output :+ (number * input.head.length))
   }
 
-  def flag(input: Array[String], output: Array[String] = Array()): Array[String] = {
+  def consumeFlag(input: Array[String], output: Array[String] = Array()): Array[String] = {
     if (input.isEmpty) output
     else {
       input.head match {
-        case "0" => consume(input.tail, output, "0")
-        case "00" => consume(input.tail, output, "1")
+        case "0" => consumeNumber(input.tail, output, "0")
+        case "00" => consumeNumber(input.tail, output, "1")
       }
     }
   }
@@ -16,6 +16,6 @@ object Main extends App {
   val file = scala.io.Source.fromFile(args(0))
   val lines = file.getLines.filter(_.length > 0)
   for (line <- lines) {
-    println(Integer.parseInt(flag(line.split(" ")).mkString("").dropWhile(_ == '0'), 2))
+    println(BigInt(consumeFlag(line.split(" ")).mkString(""), 2))
   }
 }
