7438238dd3aa63564d6e423bc237ecfd0576f162
Jordy Moos <jordy@pbwebmedia.com>
2016-05-20 14:19:45 +0200

    Challenge 93

---
+++ scala/93/Main.scala
@@ -0,0 +1,7 @@
+object Main extends App {
+  val file = scala.io.Source.fromFile(args(0))
+  val lines = file.getLines.filter(_.length > 0)
+  for (line <- lines) {
+    println(line.split(" ").map(_.capitalize).mkString(" "))
+  }
+}
